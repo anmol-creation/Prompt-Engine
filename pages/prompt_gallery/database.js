5e786a793f6e9741f4bdedbadbad07329926a280
Anmol Agarwal <creativeanmolblogs@gmail.com>
2026-01-16 16:12:37 +0530

    Update database.js

--- pages/prompt_gallery/database.js
+++ pages/prompt_gallery/database.js
@@ -2,9 +2,9 @@ export const promptDatabase = {
     "Realism & Portraits": [
         {
             id: "realism_1",
-            ai_image: "https://via.placeholder.com/300?text=Portrait+Old+Man",
-            ref_image: "https://via.placeholder.com/300?text=Ref+Old+Man",
-            prompt: "Cinematic portrait of an old man with deep wrinkles, natural lighting, 8k resolution, highly detailed."
+            ai_image: "https://res.cloudinary.com/daxgt0qfj/image/upload/v1768560133/file_00000000c5287207b074b208cd79ce1d_fcyzf8.png",
+            ref_image: "https://res.cloudinary.com/daxgt0qfj/image/upload/v1768559930/file_00000000b6ec7207bb0905d78c71f988_bo8ya6.png",
+            prompt: "on a city rooftop with night lights in the background wearing Semi-Formal Outfit, blue top and beige bottom wearing Jacket in neutral color standing holding jacket effortlessly over one shoulder, cool attitude wearing Brown Derby Shoes wearing round sunglasses wearing earbuds Preserve the subject's identity and image quality."
         },
         {
             id: "realism_2",
